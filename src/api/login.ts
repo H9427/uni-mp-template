1a15f9d0a709ea07b3fa1392247f35a3caf5c022
hbin <11323063@qq.com>
2023-11-16 17:57:10 +0800

    修改用户模块功能

--- src/api/login.ts
+++ src/api/login.ts
@@ -19,7 +19,29 @@ export const postLoginWxMinSimpleAPI = (phoneNumber: string) => {
     method: 'POST',
     url: '/user/login/wxMin/simple',
     data: {
-      phoneNumber: 17327712015
+      phoneNumber
     }
+  })
+}
+
+/**
+ * 获取个⼈信息
+ */
+export const getMemberProfileAPI = () => {
+  return http<ProfileDetail>({
+    method: 'GET',
+    url: '/user/profile'
+  })
+}
+
+/**
+ * 修改个⼈信息
+ * @param data 请求体参数
+ */
+export const putMemberProfileAPI = (data: ProfileParams) => {
+  return http<ProfileDetail>({
+    method: 'PUT',
+    url: '/user/profile',
+    data
   })
 }
